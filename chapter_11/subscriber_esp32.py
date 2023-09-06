e75dd3e17c8d6362922a4a09dc3a5b0f707faab8
ckboa <48851053+ckboa@users.noreply.github.com>
2023-09-06 20:57:04 +0700

    Update subscriber_esp32.py

--- chapter_11/subscriber_esp32.py
+++ chapter_11/subscriber_esp32.py
@@ -1,1 +1,21 @@
-import timefrom umqtt.simple2 import MQTTClient# Received messages from subscriptions will be delivered to this callbackdef sub_callback(topic, msg, retain, dup):    print("t:{} m:{}".format(topic, msg))client = MQTTClient("esp32_sub", "192.168.0.52", port=1883,                    user=None, password=None, keepalive=30,                    ssl=False, ssl_params={})                    client.set_callback(sub_callback)                   client.connect()client.subscribe(b"#")count = 0for count in range(10):     client.check_msg()    time.sleep(3)client.disconnect()
+import time
+from umqtt.simple2 import MQTTClient
+
+
+# Received messages from subscriptions will be delivered to this callback
+def sub_callback(topic, msg, retain, dup):
+    print("t:{} m:{}".format(topic, msg))
+
+
+client = MQTTClient("esp32_sub", "192.168.0.52", port=1883,
+                    user=None, password=None, keepalive=30,
+                    ssl=False, ssl_params={})
+                    
+client.set_callback(sub_callback)                   
+client.connect()
+client.subscribe(b"#")
+count = 0
+for count in range(10): 
+    client.check_msg()
+    time.sleep(3)
+client.disconnect()
